55d8d067cf7087ffe9fc570145f79c4aa948b5bc
Leveson Cocarell <Lcocarell2816@conestogac.on.ca>
2018-09-12 14:07:46 -0400

    fromatted charts

--- bi1test1/bi1test1/Form1.cs
+++ bi1test1/bi1test1/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace bi1test1
 {
-    public partial class Form1 : Form
+    public partial class BusinessIntelligenceCharts : Form
     {
-        public Form1()
+        public BusinessIntelligenceCharts()
         {
             InitializeComponent();
         }
