ff77d6232ba398af4cbc6114fe89017b1e651392
Leveson Cocarell <Lcocarell2816@conestogac.on.ca>
2018-09-23 14:51:01 -0400

    done commenting

--- bi1test1/bi1test1/BI_Assignment_01.cs
+++ bi1test1/bi1test1/BI_Assignment_01.cs
@@ -6,7 +6,8 @@
 * DESCRIPTION :
 * This file contains the source code for an Win Forms application that demonstrates the 
 * use of the Chart control to visualize data. It demonstrates the following four charts: pie chart,
-* line chart, control chart, Pareto Diagram.
+* line chart, control chart, Pareto Diagram. Pareto chart. Pareto chart creation was based on example from Microsoft: 
+* https://code.msdn.microsoft.com/Samples-Environments-for-b01e9c61
 */
 
 using System;
@@ -26,32 +27,25 @@ namespace bi1test1
         DataTable dataTable_Control = new DataTable();
         DataTable dataTable_Pareto = new DataTable();
 
-  
         /*
         * Method     : BusinessIntelligenceCharts()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N/A
+        * Description: Initialize form, create and format charts and data grid views (which control chart)
         */
         public BusinessIntelligenceCharts()
         {
-            // Format form for UI controls
+            // Format form with UI controls
             MaximizeBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 
-            // Initialize data
+            // Initialize data for charts
             InitializeComponent();
             InitializeCharts();
             InitializeDataGrids();
-            InitializeControlLimits(); 
         }
 
-      
         /*
         * Method     : InitializeCharts()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize charts for all four tabs.
         */
         private void InitializeCharts()
         {
@@ -63,9 +57,7 @@ namespace bi1test1
 
         /*
         * Method     : InitializeDataGrids()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize each data for match chart
         */
         private void InitializeDataGrids()
         {
@@ -77,9 +69,7 @@ namespace bi1test1
 
         /*
         * Method     : InitializePieChart()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Format pie chart in first tab. Set UI controls and add title and chart type. 
         */
         private void InitializePieChart()
         {
@@ -90,23 +80,41 @@ namespace bi1test1
         }
 
         /*
+       * Method     : InitializeLineChart()
+       * Description: Format and set chart as a line chart in second tab.  Set UI controls and add title and chart type. 
+       */
+        private void InitializeLineChart()
+        {
+            chart_LineChart.Titles.Add("Line Chart");
+            chart_LineChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart_LineChart.Series[0].MarkerStyle = MarkerStyle.Circle;
+        }
+
+        /*
         * Method     : InitializeControlChart()
-        * Description: In ortder to initialize the control chart, I used horizontal strip lines Sourced from https://stackoverflow.com/questions/21990022/add-horizontal-line-to-chart-in-c-sharp
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Format and set chart as a Control chart in third tab. In order to initialize the control chart, I used horizontal strip 
+        * lines as control lines. These can be manually controlled by user via the use of numeric up down controllers. 
+        * This method was sourced from https://stackoverflow.com/questions/21990022/add-horizontal-line-to-chart-in-c-sharp
         */
         private void InitializeControlChart()
         {
+            // Initialize control lines for control chart via numeric up down controls. This allows 
+            // user to have a better idea of what approx values they should input. 
+            numericUpDown_ControlLine.Value = 45;
+            numericUpDown_UpperControlLimit.Value = 90;
+            numericUpDown_LowerControlLimit.Value = 10;
+
+            // Control line numeric up down controls are labeled to correlate with color on bar 
             label_ControlLine.ForeColor = System.Drawing.Color.Black;
             label_LowerControlLimit.ForeColor = System.Drawing.Color.Green;
             label_UpperControlLimit.ForeColor = System.Drawing.Color.Red;
 
             chart_ControlChart.Titles.Add("Control Chart");
 
-            // rename series one 
+            // Set base chart for control chart as line chart  
             chart_ControlChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
 
-            // add circle style marks 
+            // add circle style marks to line 
             chart_ControlChart.Series[0].MarkerStyle = MarkerStyle.Circle;
 
             // Add center control line (using horizontal strip line) to chart
@@ -133,32 +141,16 @@ namespace bi1test1
             stripline_UpperControlLimit.BackColor = Color.Red;
             chart_ControlChart.ChartAreas[0].AxisY.StripLines.Add(stripline_UpperControlLimit);
 
-            // subscribe method to numericUpDown events to control strip lines on charts
+            // subscribe method to numericUpDown events to allow user to control strip lines on charts manually
             numericUpDown_ControlLine.ValueChanged += NumericUpDown_ControlLine_ValueChanged;
             numericUpDown_LowerControlLimit.ValueChanged += NumericUpDown_LowerControlLimit_ValueChanged;
             numericUpDown_UpperControlLimit.ValueChanged += NumericUpDown_UpperControlLimit_ValueChanged;
         }
 
         /*
-         * Method     : InitializeControlLimits()
-         * Description: Assign initial values to control chart (strip lines) to provide a 
-         * guideline for user to start. 
-         * Parameters : N/A
-         * Returns    : N?A
-        */
-        private void InitializeControlLimits()
-        {
-            numericUpDown_ControlLine.Value = 45;
-            numericUpDown_UpperControlLimit.Value = 90;
-            numericUpDown_LowerControlLimit.Value = 10;
-        }
-
-        /*
         * Method     : InitializeParetoChart()
-        * Description: Add series to chart which will be used to construct Pareto chart. Based on example from
-        * Microsoft : 
-        * Parameters : N/A
-        * Returns    : N/A
+        * Description: Format and set chart as a Pareto chart in third tab. 
+        * Add series to chart (column) which will be used to construct Pareto source chart (for line chart).
         */
         private void InitializeParetoChart()
         { 
@@ -168,7 +160,7 @@ namespace bi1test1
             // Add other series (Target Line) to chart 
             chart_ParetoChart.Series.Add("Target Line");
 
-            // Format appearance of 'control line'
+            // Format appearance of 'control line' for Pareto chart 
             chart_ParetoChart.Series["Target Line"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             chart_ParetoChart.Series["Target Line"].IsValueShownAsLabel = true;
             chart_ParetoChart.Series["Target Line"].MarkerColor = Color.Red;
@@ -178,28 +170,14 @@ namespace bi1test1
             chart_ParetoChart.Series["Target Line"].LabelFormat = "0.#";  // format with one decimal and leading zero
         }
 
-
-        /*
-        * Method     : InitializeLineChart()
-        * Description: Format and set chart as a line chart.
-        * Parameters : N/A
-        * Returns    : N?A
-        */
-        private void InitializeLineChart()
-        {
-            chart_LineChart.Titles.Add("Line Chart");
-            chart_LineChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            chart_LineChart.Series[0].MarkerStyle = MarkerStyle.Circle;
-        }
-
         /*
         * Method     : initializeDataGridView_PieChart()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize data and bind datagrid view to pie chart. This happens in a row changed event 
+        * which is added in this method as well.
         */
         private void initializeDataGridView_PieChart()
         {
+            // Create X and Y value members for pie chart series 
             chart_PieChart.Series[0].XValueMember = "X";
             chart_PieChart.Series[0].YValueMembers = "Y";
 
@@ -216,14 +194,14 @@ namespace bi1test1
             // Bind data 
             chart_PieChart.DataBind();
 
+            // add a RowChanging event handler for the table 
             dataTable_Pie.RowChanged += DataTable_Pie_RowChanged;
         }
 
         /*
         * Method     : initializeDataGridView_LineChart()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize data and bind datagrid view to line chart. This happens in a row changed event 
+        * which is added in this method as well.
         */
         private void initializeDataGridView_LineChart()
         {
@@ -250,14 +228,12 @@ namespace bi1test1
        
             // add a RowChanging event handler for the table 
             dataTable_Line.RowChanged += Dt_RowChanged;
-
         }
 
         /*
         * Method     : initializeDataGridView_ControlChart()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize data and bind datagrid view to control chart. This happens in a row changed event 
+        * which is added in this method as well. NOTES: Control lines are handles in numeric up controls.
         */
         private void initializeDataGridView_ControlChart()
         {
@@ -287,9 +263,7 @@ namespace bi1test1
 
         /*
         * Method     : initializeDataGridView_ParetoChart()
-        * Description: https://www.youtube.com/watch?v=R_VOAu2BqCk
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Initialize data and bind datagrid view to Pareto chart.
         */
         private void initializeDataGridView_ParetoChart()
         {
@@ -297,13 +271,14 @@ namespace bi1test1
             chart_ParetoChart.Series[0].XValueMember = "X";
             chart_ParetoChart.Series[0].YValueMembers = "Y";
 
-            // Label chart area axis 
-            chart_ParetoChart.ChartAreas[0].AxisX.Title = "Time";
-            chart_ParetoChart.ChartAreas[0].AxisY.Title = "Sales";
+            // Label chart area axis titles 
+            chart_ParetoChart.ChartAreas[0].AxisX.Title = "Defect Categpry";
+            chart_ParetoChart.ChartAreas[0].AxisY.Title = "Freq.";
+            chart_ParetoChart.ChartAreas[0].AxisY2.Title = "Cumulative.%";
 
+            // add column chart component 
             dataTable_Pareto.Columns.Add("X");
             dataTable_Pareto.Columns.Add("Y");
-
 
             dataGridView_ParetoChart.Anchor = AnchorStyles.Top;
             dataGridView_ParetoChart.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
@@ -314,6 +289,8 @@ namespace bi1test1
 
             // Bind data 
             chart_ParetoChart.DataBind();
+
+            // Sort column series in descending order.
             chart_ParetoChart.DataManipulator.Sort(PointSortOrder.Descending, "Series1");
 
             // add a RowChanging event handler for the table 
@@ -343,16 +320,19 @@ namespace bi1test1
 
         /*
        * Method     : DataTable_Control_RowChanged()
-       * Description: Bind data on row changing event. Buit on example from: 
-       * Parameters : N/A
-       * Returns    : N?A
+       * Description: Bind data on row changing event. 
        */
         private void DataTable_Control_RowChanged(object sender, DataRowChangeEventArgs e)
         {
             chart_ControlChart.DataBind();
         }
 
-
+        /*
+         * Method     : calculateParetoTargetLine()
+         * Description:  Find total of all in points in column chart, derive cumulative frequency percentage, and over lay
+         * Target Line series on column chart. This will create a Pareto chart. Based on example from Microsoft: 
+         * https://code.msdn.microsoft.com/Samples-Environments-for-b01e9c61.
+         */
         private void calculateParetoTargetLine()
         {
             // find the total of all points in the source series
@@ -376,8 +356,8 @@ namespace bi1test1
             // turn off the end point values of the primary X axis
             chart_ParetoChart.ChartAreas[0].AxisX.LabelStyle.IsEndLabelVisible = false;
 
+            // Need to clear points in chart series in orde to ensure Target Line does not format incorrectly. 
             chart_ParetoChart.Series["Target Line"].Points.Clear();
-
 
             // for each point in the source series find % of total and assign to series
             double percentage = 0.0;
@@ -385,77 +365,69 @@ namespace bi1test1
             // for every point in series 1 in you have a point in series 2 
             foreach (DataPoint pt in chart_ParetoChart.Series[0].Points)
             {
-                // data point y values 
+                // Calculate cumulative frequency percentage: 
                 percentage += (pt.YValues[0] / total * 100.0);
                 chart_ParetoChart.Series["Target Line"].Points.Add(Math.Round(percentage, 2));
-            }
-
-            
+            }     
         }
-
 
         /*
         * Method     : DataTable_Pareto_RowChanged()
-        * Description: Bind data on row changing event. Sourced from example from ___ 
-        * Parameters : N/A
-        * Returns    : N/A
+        * Description: Calculate Pareto target line, then bind data, then sort data descending. 
         */
         private void DataTable_Pareto_RowChanged(object sender, DataRowChangeEventArgs e)
         {
-
+            // Calculate cumulative frequency percentage for target line when user enters in values to 
+            // data grid view.
             calculateParetoTargetLine();
-            chart_ParetoChart.DataBind();
-            // sort the data in the series to be by values in descending order
-            chart_ParetoChart.DataManipulator.Sort(PointSortOrder.Descending, "Series1");
 
-            //dataTable_Pareto.Rows.Clear();
-            //chart_ParetoChart.DataBind();
+            // Bind data from gridview to Pareto chart 
+            chart_ParetoChart.DataBind();
+
+            // Sort the data in the series to be by values in descending order. 
+            // Note: This has to be done here and in initalization or column chart will not order correctly. 
+            chart_ParetoChart.DataManipulator.Sort(PointSortOrder.Descending, "Series1");
         }
 
 
         /*
-        * Method     : 
-        * Description: NumericUpDown_UpperControlLimit_ValueChanged
-        * Parameters : N/A
-        * Returns    : N?A
+        * Method     : NumericUpDown_UpperControlLimit_ValueChanged
+        * Description: Access numeric up down value inputed by user, then assign it's value to be an interval for stripline on 
+        * control chart.  In this case it is the Upper Control Limit for control chart. 
         */
         private void NumericUpDown_UpperControlLimit_ValueChanged(object sender, EventArgs e)
         {
             decimal upperLineLimitValue = numericUpDown_UpperControlLimit.Value;
-            chart_ControlChart.ChartAreas[0].AxisY.StripLines[2].IntervalOffset = decimal.ToDouble(upperLineLimitValue); // average value of the
+            chart_ControlChart.ChartAreas[0].AxisY.StripLines[2].IntervalOffset = decimal.ToDouble(upperLineLimitValue); 
 
         }
 
         /*
         * Method     : 
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description:  Access numeric up down value inputed by user, then assign it's value to be an interval for stripline on 
+        * control chart. In this case it is the Lower Control Limit for control chart. 
         */
         private void NumericUpDown_LowerControlLimit_ValueChanged(object sender, EventArgs e)
         {
             decimal lowerLineLimitValue = numericUpDown_LowerControlLimit.Value;
-            chart_ControlChart.ChartAreas[0].AxisY.StripLines[1].IntervalOffset = decimal.ToDouble(lowerLineLimitValue); // average value of the y axis; eg:
+            chart_ControlChart.ChartAreas[0].AxisY.StripLines[1].IntervalOffset = decimal.ToDouble(lowerLineLimitValue); 
         }
 
         /*
-        * Method     : 
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Method     : NumericUpDown_ControlLine_ValueChanged
+        * Description: Access numeric up down value inputed by user, then assign it's value to be an interval for stripline on 
+        * control chart.  In this case it is the Center Control for control chart. 
         */
         private void NumericUpDown_ControlLine_ValueChanged(object sender, EventArgs e)
         {
             decimal controlLineValue = numericUpDown_ControlLine.Value;
-            chart_ControlChart.ChartAreas[0].AxisY.StripLines[0].IntervalOffset = decimal.ToDouble(controlLineValue); // average value of the y axis; eg:
-            // figure out where to add in index 
+            chart_ControlChart.ChartAreas[0].AxisY.StripLines[0].IntervalOffset = decimal.ToDouble(controlLineValue); 
         }
 
         /*
         * Method     : button_ClearCharts_Click()
-        * Description: 
-        * Parameters : N/A
-        * Returns    : N?A
+        * Description: Allow users to clear all charts in tabs with button. Functions by clearing series points in charts 
+        * and binding them to respective charts again.
         */
         private void button_ClearCharts_Click(object sender, EventArgs e)
         {
